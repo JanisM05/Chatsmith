4a419d7156919aa66ed1ce4ec8d5068478b53506
jamurpf <janis.murpf@gmail.com>
2021-12-09 16:15:13 +0100

    Projektdateien hinzufügen.

---
+++ Chat_Projekt_Blj/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Chat_Projekt_Blj
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_login = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_errors = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_login
+            // 
+            this.lbl_login.AutoSize = true;
+            this.lbl_login.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lbl_login.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_login.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_login.Location = new System.Drawing.Point(10, 9);
+            this.lbl_login.Name = "lbl_login";
+            this.lbl_login.Padding = new System.Windows.Forms.Padding(75, 10, 75, 10);
+            this.lbl_login.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbl_login.Size = new System.Drawing.Size(278, 46);
+            this.lbl_login.TabIndex = 0;
+            this.lbl_login.Text = "Chatsmith";
+            this.lbl_login.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Benutzername";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Arial Nova", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(43, 97);
+            this.txt_username.MaximumSize = new System.Drawing.Size(250, 50);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(214, 23);
+            this.txt_username.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Passwort";
+            // 
+            // txt_password
+            // 
+            this.txt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Location = new System.Drawing.Point(43, 165);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(214, 24);
+            this.txt_password.TabIndex = 4;
+            this.txt_password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(123, 216);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 27);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // lbl_errors
+            // 
+            this.lbl_errors.AutoSize = true;
+            this.lbl_errors.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_errors.Location = new System.Drawing.Point(39, 261);
+            this.lbl_errors.Name = "lbl_errors";
+            this.lbl_errors.Size = new System.Drawing.Size(0, 16);
+            this.lbl_errors.TabIndex = 6;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(300, 316);
+            this.Controls.Add(this.lbl_errors);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label lbl_errors;
+    }
+}
+
---
+++ Chat_Projekt_Blj/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat_Projekt_Blj
+{
+    public partial class frmLogin : Form
+    {
+
+        Timer timer = new Timer();
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Tick += Timer_Tick;
+            timer.Interval = 5000;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+        }
+
+
+        private void frmLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop(); MessageBox.Show("Benutzer schliesst formular.");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (txt_username.Text != "" && txt_password.Text != "")
+            {
+                
+            }
+            else
+            {
+                lbl_errors.Text = "Sie müssen beide Felder ausfüllen!";
+            }
+        }
+    }
+}
